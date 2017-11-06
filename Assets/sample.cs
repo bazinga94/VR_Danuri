080f838be2a61868708323150426f19468780018
bazinga94 <bazinga94@naver.com>
2017-11-06 20:17:01 +0900

    project start!
    팡훈이가 상자에 움직임만 구현해둔 상태
    좌회전, 우회전, 속도 조절 가능
    Hello Danuri~

---
+++ Assets/sample.cs
@@ -0,0 +1,14 @@
+public class sample: EActor
+{
+	public override int OnMessage(string msg, int arg0, Vector4 arg1, Vector4 arg2)
+	{
+		//¿Ã∫•∆Æ
+		return 0;
+	}
+
+	public override int Update()
+	{
+	Log("hello danuri",0,0.0f);
+	return 0;
+	}
+}
---
+++ Assets/script/car.py
@@ -0,0 +1,56 @@
+import math
+class car(Actor.Actor):
+
+    speed = 0.005
+    A = 0.00
+    B = 0.00
+    PI = 3.141592
+    
+
+    def __init__(self):
+        self.car=Container(0)
+        return
+    def OnCreate(self,uid):
+        self.cartrans = self.car.FindComponentByType("TransformGroup")
+        self.carpos = self.cartrans.GetPosition()
+        self.carrot = self.cartrans.GetRotation()
+        return
+    def OnDestory(self):
+        return
+    def OnEnable(self):
+        return
+    def OnDisable(self):
+        return
+    def Update(self):
+        self.carpos.x += self.speed*(math.cos((math.pi / 2) - (math.pi*(self.B/180))))
+        self.carpos.z += self.speed*(math.sin((math.pi / 2) - (math.pi*(self.B/180))))
+        self.cartrans.SetPosition(self.carpos)
+        if (self.A!=self.B):
+            if (self.A < self. B):
+                self.B -= 1
+                self.cartrans.Rotate(-1,0,(0,1,0))
+            else :
+                self.B += 1
+                self.cartrans.Rotate(1,0,(0,1,0))
+            
+            
+        
+        return
+    def OnMessage(self, msg, number, Vector4_lparm, Vector4_wparam):
+        if (msg == "KeyDown"):
+            if( number == 0x41): #"A"
+                self.A -= 1
+            elif( number == 0x44): #"D"
+                self.A += 1
+                #self.cartrans.Rotate(30,0,(0,1,0))
+            elif( number == 0x57): #"W"
+                self.speed += 0.005
+            elif( number == 0x53): #"S"
+                self.speed -= 0.005
+            
+                
+            
+        return
+
+
+  
